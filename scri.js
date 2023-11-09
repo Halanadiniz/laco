cb565e6e344171987178a5a05032392bb6e0bc87
Halanadiniz <128141932+Halanadiniz@users.noreply.github.com>
2023-11-09 08:45:39 -0300

    Add files via upload

--- scri.js
+++ scri.js
@@ -1,9 +1,17 @@
-function potencia() {
-    let n = 5;
+function potencia(){
+    let n = parseInt(numero.value);
     let pote = n;
+    if (n<0){
+        alert("numero negativo");
+    }
+    else if(n==0){
+        alert("1")
+    }
+    else{
     while (n>=1) {
-        pote = Math.pow(2, n);
+        pote = Math.pow(2,n);
         n = n - 1;
         alert(pote);
+    }
     }
 }
